b20ba19459b7e11fd794ff5545d8549ce728d00b
Mike Packard <31674103+MikePackard@users.noreply.github.com>
2017-09-19 14:43:43 -0400

    Add files via upload

---
+++ 3D Resume Template/MainControllerTemplate.js
@@ -0,0 +1,70 @@
+app.controller('MainControllerTemplate', ['$scope', function($scope) { 
+	$scope.basicInfo = {
+		name: 'Name',
+		phone: '123-456-7890',
+		email: 'EmailHere@gmail.com',
+		DOB: 'January 1st, 1995, Age 22',
+		hometown: 'Arlington',
+		homestate_abrv: 'MA',
+		headshot: 'Please_read.jpg',
+		bio: 'Howdy! Thanks for checking out my 3D resume. It has all the info on my normal resume, plus a ton more. Try clicking around!'
+	}
+	$scope.education = {
+		school: 'My College',
+		location: 'my college location',
+		major: 'my major',
+		minor: 'my minor',
+		gpa: 'my gpa',
+		date_graduate: 'Graduated May, 2017',
+		picture: 'Please_read.jpg',
+		bio: 'At my school I did a lot of things. My favorite class was _____ because _____. I chose to major in my major because of these reasons. What I am most proud of from my time at this school is ______'
+	}
+	$scope.experience1 = {
+		organization: 'Company Name',
+		location: 'location of company',
+		title: 'Position Title',
+		date_start: 'This date',
+		date_end: 'That date',
+		desc1: 'This was one of my main responsibilities',
+		desc2: 'This was my biggest accomplishment',
+		desc3: 'This is something else impressive about this job.',
+		picture: 'Please_read.jpg',
+		bio: 'This is more info about this job. My favorite thing about it was that it was next to a Dunkin Donuts. My boss was nice but very demanding, and I felt like I could have done better if things had been different in this way. This is what this says about me as a person. Also, here are the most important things I learned in this position.'
+	}
+	$scope.experience2 = {
+		organization: 'Company Name',
+		location: 'location of company',
+		title: 'Position Title',
+		date_start: 'This date',
+		date_end: 'That date',
+		desc1: 'This was one of my main responsibilities',
+		desc2: 'This was my biggest accomplishment',
+		desc3: 'This is something else impressive about this job.',
+		picture: 'Please_read.jpg',
+		bio: 'This is more info about this job. My favorite thing about it was that it was next to a Dunkin Donuts. My boss was nice but very demanding, and I felt like I could have done better if things had been different in this way. This is what this says about me as a person. Also, here are the most important things I learned in this position.'
+	}
+	$scope.experience3 = {
+		organization: 'Company Name',
+		location: 'location of company',
+		title: 'Position Title',
+		date_start: 'This date',
+		date_end: 'That date',
+		desc1: 'This was one of my main responsibilities',
+		desc2: 'This was my biggest accomplishment',
+		desc3: 'This is something else impressive about this job.',
+		picture: 'Please_read.jpg',
+		bio: 'This is more info about this job. My favorite thing about it was that it was next to a Dunkin Donuts. My boss was nice but very demanding, and I felt like I could have done better if things had been different in this way. This is what this says about me as a person. Also, here are the most important things I learned in this position.'
+	}
+	$scope.experience4 = {
+		organization: 'Company Name',
+		location: 'location of company',
+		title: 'Position Title',
+		date_start: 'This date',
+		date_end: 'That date',
+		desc1: 'This was one of my main responsibilities',
+		desc2: 'This was my biggest accomplishment',
+		desc3: 'This is something else impressive about this job.',
+		picture: 'Please_read.jpg',
+		bio: 'This is more info about this job. My favorite thing about it was that it was next to a Dunkin Donuts. My boss was nice but very demanding, and I felt like I could have done better if things had been different in this way. This is what this says about me as a person. Also, here are the most important things I learned in this position.'
+	}
+}]);
---
+++ 3D Resume Template/js/app.js
@@ -0,0 +1,1 @@
+var app = angular.module("my3DResume", []);
